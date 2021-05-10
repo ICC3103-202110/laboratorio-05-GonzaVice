eae9778243fe6603676017e7fc7d88f860b411bd
GonzaVice <givicente@miuandes.cl>
2021-05-10 18:46:14 -0400

    Prototype

--- Lab5.js
+++ Lab5.js
@@ -1,1 +1,9 @@
-console.log("Calculadora.");
+const prompt = require('prompt-sync')();
+
+const total_bill = (bill_init, add_percentage) => (bill_init + (bill_init * add_percentage));
+
+let bill = parseInt(prompt('Bill Amount?: '));
+let tip_percent = parseInt(prompt('Tip(%)?: '));
+let bill_total = total_bill(bill, tip_percent);
+
+console.log('Bill Total = ' + bill_total);
