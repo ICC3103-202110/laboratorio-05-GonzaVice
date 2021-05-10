6336f0f34e1ac03c069d20a4dd00571d41c93941
GonzaVice <givicente@miuandes.cl>
2021-05-10 19:15:34 -0400

    testCases

--- Lab5.js
+++ Lab5.js
@@ -1,9 +1,31 @@
+const figlet = require('figlet');
 const prompt = require('prompt-sync')();
+const chalk = require('chalk');
+const { printTable } = require('console-table-printer');
 
-const total_bill = (bill_init, add_percentage) => (bill_init + (bill_init * add_percentage));
+const total_bill = (bill_init, add_percentage) => (bill_init + (bill_init * add_percentage * 0.01));
 
-let bill = parseInt(prompt('Bill Amount?: '));
-let tip_percent = parseInt(prompt('Tip(%)?: '));
+let bill = parseInt(prompt(chalk.blue.bold('Bill Amount?: ')));
+let tip_percent = parseInt(prompt(chalk.blue.bold('Tip(%)?: ')));
 let bill_total = total_bill(bill, tip_percent);
 
-console.log('Bill Total = ' + bill_total);
+console.log(chalk.red.bold('Bill Total = ') + bill_total);
+
+
+figlet('Tip Calculator App', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+const testCases = [{
+    'Bill Amount': '$' + bill,
+    'Tip (%)': tip_percent + '%',
+    'Tip': '$' + (bill * tip_percent * 0.01),
+    'Total': '$' + bill_total}
+  ];
+
+  printTable(testCases);
